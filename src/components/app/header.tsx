fa5155c04bae0df5a3e3701d340bcabcb43e03de
평모 <kpm9602@gmail.com>
2025-09-09 11:48:30 +0000

    세로 스크롤하면 헤더가 일부 컴포넌트에 가려지는데 z index 조절이 필요할 듯

--- src/components/app/header.tsx
+++ src/components/app/header.tsx
@@ -18,7 +18,7 @@ export default function Header() {
   const { user, logout } = useAuth();
 
   return (
-    <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
+    <header className="sticky top-0 z-10 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
       <Link
         href="#"
         className="flex items-center gap-2 text-lg font-semibold md:text-base"
